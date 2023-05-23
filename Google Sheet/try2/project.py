82faddeef029b54df7fbc2c23130081c99a78788
maggyho612 <maggyho612@gmail.com>
2023-05-23 10:12:23 +0800

    read_write_sheet
    寫了一個新的程式，用service account可以讀跟寫

---
+++ Google Sheet/try2/project.py
@@ -0,0 +1,29 @@
+
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SERVICE_ACCOUNT_FILE = 'keys.json'
+
+creds = None
+creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+
+# The ID spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1tlD0m4tZSL10vs30ds5Rjia5_Oa-JdTHAeKEhBtTKRk'
+
+service = build('sheets', 'v4', credentials=creds)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range='0523 demo Title!A1:E6').execute()
+values = result.get('values', [])
+
+put_in = [["05/23/2012", 2000], ["05/24/2012", 5000], ["05/24/2012", 5000]]
+
+request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range='Sheet2!B2', valueInputOption="USER_ENTERED", body={"values":put_in}).execute()
+
+
+

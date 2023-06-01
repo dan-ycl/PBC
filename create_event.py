4468151eaa1010169b2a89b89ede685ffc44ab49
cmxzero <hjuo37689@gmail.com>
2023-06-01 11:27:08 +0800

    Update create_event.py

--- create_event.py
+++ create_event.py
@@ -31,27 +31,27 @@ def create_event(meeting_name, start, end, CREDENTIALS_FILE, attend_list):
 
 
 
-CREDENTIALS_FILE = './Google Calendar API/credentials_oauth.json' 
-meeting_name = '05/25 Meeting'
-# 測試版的會議時間：creates one hour event tomorrow 10 AM IST
-d = datetime.now().date()
-tomorrow = datetime(d.year, d.month, d.day, 10)+timedelta(days=1)
-start = tomorrow.isoformat()
-end = (tomorrow + timedelta(hours=2)).isoformat()
+# CREDENTIALS_FILE = './Google Calendar API/credentials_oauth.json' 
+# meeting_name = '05/25 Meeting'
+# # 測試版的會議時間：creates one hour event tomorrow 10 AM IST
+# d = datetime.now().date()
+# tomorrow = datetime(d.year, d.month, d.day, 10)+timedelta(days=1)
+# start = tomorrow.isoformat()
+# end = (tomorrow + timedelta(hours=2)).isoformat()
 
 
-print(start)
-print(end)
+# print(start)
+# print(end)
 
-# email 參與者 (小奇測試個人email帳號 都ok)
-email_list = ['hjuo37689@gmail.com']
-attend_list = [0] * len(email_list)
+# # email 參與者 (小奇測試個人email帳號 都ok)
+# email_list = ['hjuo37689@gmail.com']
+# attend_list = [0] * len(email_list)
 
-for i in range(len(email_list)):
-    attend_list[i] = {'email': email_list[i]}
+# for i in range(len(email_list)):
+#     attend_list[i] = {'email': email_list[i]}
 
-# Function 1：host 發起會議
-# 需要下載 cal_setup.py 、 oauth權限的 crendential.json 和 pip google oauth
-# cal_setup.py 請和create.py放在同一資料夾
-# 共須傳入五個參數
-host_setup = create_event(meeting_name, start, end, CREDENTIALS_FILE, attend_list)
+# # Function 1：host 發起會議
+# # 需要下載 cal_setup.py 、 oauth權限的 crendential.json 和 pip google oauth
+# # cal_setup.py 請和create.py放在同一資料夾
+# # 共須傳入五個參數
+# host_setup = create_event(meeting_name, start, end, CREDENTIALS_FILE, attend_list)
